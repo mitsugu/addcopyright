20445d19e462dee31af6cfc78aab5a9385325115
mitsugu oyama <mitsugu.oyama@gmail.com>
2025-02-17 15:18:13 +0900

    Migration urfave/cli/v2 to urfave/cli/v3

--- addcopyright.go
+++ addcopyright.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v3"
 )
 
 type Config struct {
@@ -33,67 +35,7 @@ func (c *Config) readConfig(path string) (*Config, error) {
 
 	return c, nil
 }
-
 var config Config
-
-func main() {
-	app := &cli.App{
-		Name:    "addcopyright",
-		Usage:   "Add copyright information to an image's EXIF data",
-		Version: "v1.1.1",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "config",
-				Aliases: []string{"c"},
-				Usage:   "Load configuration from `FILE`",
-				Value:   "addcopyright.json",
-			},
-			&cli.StringFlag{
-				Name:     "input",
-				Aliases:  []string{"i"},
-				Usage:    "Path to the input image file",
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "output",
-				Aliases:  []string{"o"},
-				Usage:    "Path to the output image file",
-				Required: true,
-			},
-		},
-		Action: func(c *cli.Context) error {
-			inputFilePath := c.String("input")
-			outputFilePath := c.String("output")
-
-			configPath := c.String("config")
-			if configPath == "" {
-				configPath = "addcopyright.json"
-			}
-			config, err := config.readConfig(configPath)
-			if err != nil {
-				return fmt.Errorf("error reading config file: %w", err)
-			}
-
-			if _, err := os.Stat(outputFilePath); os.IsNotExist(err) {
-				if err := copyFile(inputFilePath, outputFilePath); err != nil {
-					return fmt.Errorf("error copying file: %w", err)
-				}
-			}
-
-			cmd := exec.Command(config.ExifToolPath, "-Copyright="+config.Copyright, "-Artist="+config.Copyright, outputFilePath)
-			if err := cmd.Run(); err != nil {
-				return fmt.Errorf("error executing exiftool: %w", err)
-			}
-
-			fmt.Println("EXIF data updated successfully!")
-			return nil
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		fmt.Println(err)
-	}
-}
 
 func copyFile(src, dst string) error {
 	if src == dst {
@@ -127,3 +69,63 @@ func copyFile(src, dst string) error {
 	_, err = io.Copy(destinationFile, sourceFile)
 	return err
 }
+
+func main() {
+	app := &cli.Command {
+		Name:  "addcopyright",
+		Usage: "Add copyright information to an image's EXIF data",
+		Version: "v1.2.0",
+        Flags: []cli.Flag{
+			&cli.StringFlag{
+            	Name:    "config",
+            	Aliases: []string{"c"},
+            	Usage:   "Load configuration from `FILE`",
+            	Value:   "addcopyright.json",
+            },
+			&cli.StringFlag{
+            	Name:     "input",
+            	Aliases:  []string{"i"},
+            	Usage:    "Path to the input image file",
+            	Required: true,
+            },
+			&cli.StringFlag{
+            	Name:     "output",
+            	Aliases:  []string{"o"},
+            	Usage:    "Path to the output image file",
+            	Required: true,
+            },
+        },
+		Action: func(ctx context.Context, c *cli.Command) error {
+			inputFilePath := c.String("input")
+			outputFilePath := c.String("output")
+
+			configPath := c.String("config")
+			config, err := config.readConfig(configPath)
+			if err != nil {
+				return fmt.Errorf("error reading config file: %w", err)
+			}
+
+			if _, err := os.Stat(outputFilePath); os.IsNotExist(err) {
+				if err := copyFile(inputFilePath, outputFilePath); err != nil {
+					return fmt.Errorf("error copying file: %w", err)
+				}
+			}
+
+			cmd := exec.Command(config.ExifToolPath, "-Copyright="+config.Copyright, "-Artist="+config.Copyright, outputFilePath)
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				log.Fatalf("error executing exiftool: %v\nOutput: %s", err, output)
+				return fmt.Errorf("error executing exiftool: %w", err)
+			}
+
+			fmt.Println("EXIF data updated successfully!")
+			fmt.Println(string(output))
+			return nil
+		},
+	}
+
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		log.Println(err)
+	}
+}
+
